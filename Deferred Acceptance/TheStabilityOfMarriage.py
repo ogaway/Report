dd17d5eb7298b14536c44555fd10c6d00bc95df9
小川慶将 <yshms_o@yahoo.co.jp>
2015-06-08 05:09:45 +0900

    revise the part of changing her partner

--- Deferred Acceptance/TheStabilityOfMarriage.py
+++ Deferred Acceptance/TheStabilityOfMarriage.py
@@ -4,13 +4,13 @@ femalenum = 4
 single = []
 married = {}
 mprefer = [[1, 2, 3, 4],
-[3, 2, 1, 4],
-[1, 2, 4, 3],
-[3, 1, 4, 2]]
+           [3, 2, 1, 4],
+           [1, 2, 4, 3],
+           [3, 1, 4, 2]]
 fprefer = [[1, 2, 3, 4],
-[2, 1, 4, 3],
-[2, 3, 1, 4],
-[1, 4, 3, 2]]
+           [2, 1, 4, 3],
+           [2, 3, 1, 4],
+           [1, 4, 3, 2]]
 
 for i in range(malenum + femalenum):
     single.append(i)
@@ -25,15 +25,15 @@ while True:
                     single.remove(femaleid+malenum)
                     married.update({femaleid+1: i+1})
                     break
-                elif fprefer[femaleid][married[femaleid+1]-1] > fprefer[femaleid][i]:
+                elif fprefer[femaleid].index(married[femaleid+1]) > fprefer[femaleid].index(i+1):
                     single.append(married[femaleid+1]-1)
                     single.remove(i)
-                    married.update({femaleid: i+1})
+                    married.update({femaleid+1: i+1})
                     break
                 else:
                     counter += 1
     if len(single) == 0:
         break
-married = married.items ()
+married = married.items()
 for (i, j) in married:
-    print "女性No.%sは男性No.%sと結ばれました。"%(i, j)
+    print "女性No.%sは男性No.%sと結ばれました。" % (i, j)
